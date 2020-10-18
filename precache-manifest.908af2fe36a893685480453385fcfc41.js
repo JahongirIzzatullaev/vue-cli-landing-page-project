dba44cc0041f1e9a66487ff62247bdf791743cc4
JahongirIzzatullaev@m <jahongirizzatullaev@mail.ru>
2020-10-18 16:54:15 +0500

    deploy

---
+++ precache-manifest.908af2fe36a893685480453385fcfc41.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38374146b1fde7198b40",
+    "url": "/vue-cli-project/css/app.6cc8e2e4.css"
+  },
+  {
+    "revision": "cc203bca14a6afc164d6",
+    "url": "/vue-cli-project/css/chunk-vendors.518f8f28.css"
+  },
+  {
+    "revision": "83b335dbb62171013a7655a50b0b9f77",
+    "url": "/vue-cli-project/img/0001.83b335db.png"
+  },
+  {
+    "revision": "3c786e3dd00e486a1e891fc944d3f785",
+    "url": "/vue-cli-project/img/0002.3c786e3d.png"
+  },
+  {
+    "revision": "e94edffb65e0a1ff431763a6ff4df4e0",
+    "url": "/vue-cli-project/img/0003.e94edffb.png"
+  },
+  {
+    "revision": "3b167d553b2abcd624a39f02e3f6b160",
+    "url": "/vue-cli-project/img/bg-img.3b167d55.jpg"
+  },
+  {
+    "revision": "839b874bd232024ea312941d67f6fb84",
+    "url": "/vue-cli-project/img/card-icon-svg-1.839b874b.svg"
+  },
+  {
+    "revision": "232ee911cda09abf9cff3fa54604d9ab",
+    "url": "/vue-cli-project/img/card-icon-svg-2.232ee911.svg"
+  },
+  {
+    "revision": "40e727e8a66eda700d8ad2cc3450f782",
+    "url": "/vue-cli-project/img/card-icon-svg-3.40e727e8.svg"
+  },
+  {
+    "revision": "f5a7e6c8a4fff690d3a12ed84db1c01f",
+    "url": "/vue-cli-project/img/card-icon-svg-4.f5a7e6c8.svg"
+  },
+  {
+    "revision": "2000cefa3ab28ed85f04716cc0dbd0fc",
+    "url": "/vue-cli-project/img/card-img-1.2000cefa.png"
+  },
+  {
+    "revision": "3ebc88a208f7ed9570dd4594b1a5be38",
+    "url": "/vue-cli-project/img/card-img-2.3ebc88a2.png"
+  },
+  {
+    "revision": "046e4571bf3b5230cffbc271481fda92",
+    "url": "/vue-cli-project/img/card-img-3.046e4571.png"
+  },
+  {
+    "revision": "0dfe49d5d2055d89fac52db5d962a84d",
+    "url": "/vue-cli-project/img/card-img-4.0dfe49d5.png"
+  },
+  {
+    "revision": "9b7b84ffa91e3f0a84de4a1b6d3ff929",
+    "url": "/vue-cli-project/img/furniture-1.9b7b84ff.png"
+  },
+  {
+    "revision": "aef61a29bd943d9b6ece1a62507a5024",
+    "url": "/vue-cli-project/img/furniture-2.aef61a29.png"
+  },
+  {
+    "revision": "e7b3e0d0005b6ec9e23a1329fc1566e8",
+    "url": "/vue-cli-project/img/furniture-bg.e7b3e0d0.png"
+  },
+  {
+    "revision": "f9261db3ce835dd8acd2bc7f90a1824d",
+    "url": "/vue-cli-project/img/home-1.f9261db3.png"
+  },
+  {
+    "revision": "1b55cd5caa187bfde94664f46f47e0e6",
+    "url": "/vue-cli-project/img/home-2.1b55cd5c.png"
+  },
+  {
+    "revision": "e829b337576d7744659d29714806229d",
+    "url": "/vue-cli-project/img/home-bg.e829b337.png"
+  },
+  {
+    "revision": "621658871c6200d6e695d5265da0bddd",
+    "url": "/vue-cli-project/img/logo-final.62165887.svg"
+  },
+  {
+    "revision": "e8694e5078f718f3c593fa59e1b1342f",
+    "url": "/vue-cli-project/img/our-team-girl-1.e8694e50.png"
+  },
+  {
+    "revision": "a2389f978bcb18bba5953618d8d14af5",
+    "url": "/vue-cli-project/img/our-team-girl-2.a2389f97.png"
+  },
+  {
+    "revision": "017ce723adcd3b3479d67931e9bff4a9",
+    "url": "/vue-cli-project/img/our-team-man-1.017ce723.png"
+  },
+  {
+    "revision": "e9dcb1e482f14be55c76cb41d874068b",
+    "url": "/vue-cli-project/img/our-team-man-2.e9dcb1e4.png"
+  },
+  {
+    "revision": "0b01f0bbe6bdbf6052851c5d34801067",
+    "url": "/vue-cli-project/index.html"
+  },
+  {
+    "revision": "38374146b1fde7198b40",
+    "url": "/vue-cli-project/js/app.1c0ebcd2.js"
+  },
+  {
+    "revision": "cc203bca14a6afc164d6",
+    "url": "/vue-cli-project/js/chunk-vendors.f5ebc78e.js"
+  },
+  {
+    "revision": "ce94aa2da3c0e3c2efbb4f651c51503c",
+    "url": "/vue-cli-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-cli-project/robots.txt"
+  }
+]);
